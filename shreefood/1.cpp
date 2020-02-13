426b5584362a4389bb08650b09781c0f6e162699
RishitKundariya <RishitKundariya@github.com>
2020-02-13 21:10:51 +0530

    update

---
+++ shreefood/1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+	char str;
+	printf("enter the string:");
+	scanf("%s",str)
+	for(i=1;i<=str[];i++)
+	{
+		for (int j = str[0]; j <= i ; j++)
+		{
+			printf("%s",str);
+		}
+		printf("\n");
+	}
+	return 0;
+}
